6e2050913381c4416038163a2a20c1ad55286169
fenglixa <5152195@qq.com>
2023-07-17 16:41:52 +0800

    add gradio support

---
+++ packages/components/nodes/tools/Gradio/Gradio.ts
@@ -0,0 +1,47 @@
+import { BaseLanguageModel } from 'langchain/base_language'
+import { INode, INodeData, INodeParams } from '../../../src/Interface'
+import { getBaseClasses } from '../../../src/utils'
+import { Embeddings } from 'langchain/embeddings/base'
+import { WebBrowser } from 'langchain/tools/webbrowser'
+
+class Gradio_Tools implements INode {
+    label: string
+    name: string
+    description: string
+    type: string
+    icon: string
+    category: string
+    baseClasses: string[]
+    inputs: INodeParams[]
+
+    constructor() {
+        this.label = 'Gradio Tool'
+        this.name = 'Gradio'
+        this.type = 'WebBrowser'
+        this.icon = 'Gradio.png'
+        this.category = 'Tools'
+        this.description = 'Provide the application UI with Gradio'
+        this.inputs = [
+            {
+                label: 'Language Model',
+                name: 'model',
+                type: 'BaseLanguageModel'
+            },
+            {
+                label: 'Embeddings',
+                name: 'embeddings',
+                type: 'Embeddings'
+            }
+        ]
+        this.baseClasses = [this.type, ...getBaseClasses(WebBrowser)]
+    }
+
+    async init(nodeData: INodeData): Promise<any> {
+        const model = nodeData.inputs?.model as BaseLanguageModel
+        const embeddings = nodeData.inputs?.embeddings as Embeddings
+
+        return new WebBrowser({ model, embeddings })
+    }
+}
+
+module.exports = { nodeClass: Gradio_Tools }
